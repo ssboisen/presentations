38861c5e46e8010123ba4c4064a4c78877ca07a0
Simon Skov Boisen <ssb@d60.dk>
2014-03-20 11:43:37 +0100

    Added intro to fsharp from aanug

---
+++ intro-to-functional-programming-in-fsharp/PatternMatching.fsx
@@ -0,0 +1,66 @@
+﻿(*
+  Active Patterns to multiple of some number
+  
+  Parsing regex' the awesome way!
+  
+  Capturing Integers as patterns  
+*)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let (|MULTIPLE_OF|_|) a b =
+    if b % a = 0 then Some() else None
+
+[3..1000] |> Seq.sumBy (fun x ->
+    match x with
+        | MULTIPLE_OF 3
+        | MULTIPLE_OF 5 -> x 
+        | _ -> 0)
+
+let (|ParseRegEx|_|) pattern input =
+    let m = System.Text.RegularExpressions.Regex.Match(input, pattern)
+    if m.Success 
+        then Some(List.tail [for x in m.Groups -> x.Value ])
+        else None
+
+let (|Integer|_|) input =
+    let (b, r) = System.Int32.TryParse input
+    if b then Some(r) else None
+
+let parseData input =
+    match input with
+        | ParseRegEx "(.+): (\d+)" [name; Integer age] ->
+            printfn "%s are %i år gammel" name age
+        | _ -> printfn "unable to parse numbers"
+
+parseData "Simon Skov Boisen: 29 år"
---
+++ intro-to-functional-programming-in-fsharp/TennisKata.fsx
@@ -0,0 +1,102 @@
+﻿(*
+Domain of Tennis:
+
+- Player
+- Player Points
+- State of Game:
+   - Points
+   - Advantage
+   - Deuce
+   - Game
+*)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let random = System.Random()
+
+type Player = A | B
+type PlayerPoints = Zero | Fifteen | Thirty | Fourty
+
+type Score =
+    | Points of PlayerPoints * PlayerPoints
+    | Advantage of Player
+    | Deuce
+    | Game of Player
+
+let incScore =
+    function 
+        | Zero -> Fifteen 
+        | Fifteen -> Thirty 
+        | Thirty -> Fourty 
+        | Fourty -> failwith "what?!"
+
+let normalize = 
+    function
+        | Points(Fourty, Fourty) -> Deuce 
+        | score -> score
+
+let scorePoint score point =
+    match score, point with
+    | Points(Fourty, _), A -> Game A
+    | Points(_, Fourty), B -> Game B
+    | Points(a, b), A -> normalize (Points (incScore a, b))
+    | Points(a, b), B -> normalize (Points (a, incScore b))
+    | Deuce, p -> Advantage p
+    | Advantage p1, p2 when p1 = p2 -> Game p1
+    | Advantage p1, p2 -> Deuce
+    | Game _, _ ->  score
+
+let randomWinner () =
+    if random.NextDouble() <= 0.5
+        then A
+        else B
+
+let scoreGame points =
+    Seq.scan scorePoint (Points(Zero, Zero)) points
+
+let gameStream =
+    seq { while true do
+           yield randomWinner() }
+
+gameStream 
+|> scoreGame
+|> Seq.pairwise
+|> Seq.takeWhile (function Game p, Game _ -> printfn "Player: %A won!" p; false | _ -> true)
+|> Seq.map snd
+|> List.ofSeq
